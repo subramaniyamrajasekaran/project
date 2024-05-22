c0f977c2ce281cfae40fffe5cbb45aa13d944c99
subramaniyamrajasekaran <bharathi07101998@gmail.com>
2024-05-22 23:32:07 +0530

    done

--- src/org/test/Process.java
+++ src/org/test/Process.java
@@ -5,12 +5,11 @@ public static void main(String[] args) {
 	String s="welcome to new world";
 	System.out.println(s);
 	
-<<<<<<< HEAD
-	String s2="hello udhay";
-	System.out.println(s2);
-=======
 	String s1="hello bro";
 	System.out.println(s1);
->>>>>>> feb54112c645621cdfbbc7cb57f0c3cdd9c51523
+
+	String s2="hello udhay";
+	System.out.println(s2);
+
 }
 }
