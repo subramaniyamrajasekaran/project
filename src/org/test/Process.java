94595c986f3d2129c4fc507539defbc8d851296e
subramaniyamrajasekaran <bharathi07101998@gmail.com>
2024-05-22 23:21:20 +0530

    win

--- src/org/test/Process.java
+++ src/org/test/Process.java
@@ -4,5 +4,8 @@ public class Process {
 public static void main(String[] args) {
 	String s="welcome to new world";
 	System.out.println(s);
+	
+	String s2="hello udhay";
+	System.out.println(s2);
 }
 }
